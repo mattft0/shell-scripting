3381c8d99a58994b4eb6e42eb7427f1822c92f6a
Matteo Floirat <matteo.floirat@edu.devinci.fr>
2021-12-06 14:06:14 +0100

    Exo 4

---
+++ exercise-4/salutation.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+if [ -z $1 ] || [ -z $2 ] || [ ! -z $3 ]
+then
+  echo "Usage: ./salutation.sh firstname lastname"
+  exit 1
+else
+  echo "Hello $1 $2 have a nice day!."
+  exit 0
+fi
