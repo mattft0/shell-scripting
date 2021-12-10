841df686a54ef535688fff58553c590f614c13cb
Antoine Guérin <antoine.guerin-villeaubreil@edu.devinci.fr>
2021-12-10 18:54:04 +0100

    exo6

---
+++ exercise-6/my_ansible.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+mkdir -p documents/gamez
+mkdir -p documents/images
+mkdir -p documents/work
+mkdir -p documents/work/code
+mkdir -p documents/work/plannings
+touch -a documents/gamez/csgo.exe
+touch -a documents/work/plannings/april.xlsx
+touch -a documents/work/plannings/february.xlsx
+touch -a documents/work/plannings/january.xlsx
+touch -a documents/work/plannings/march.xlsx
+touch meeting_notes.txt
