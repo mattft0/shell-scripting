ab81d8e00ac8284ec0ac8a42b249584cbd7e6bf8
Matteo Floirat <matteo.floirat@edu.devinci.fr>
2021-12-14 21:00:45 +0100

    correction exo 3

--- exercise-3/ls_ordered_with_arguments.sh
+++ exercise-3/ls_ordered_with_arguments.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
+if [ ! -z "$1" ] || [ -d "$1" ]
+then
+  ls -S -l $1
 
-echo "$(ls -Rahl)$1"
-exit 2
+elif [ ! -z "$1" ] && [ ! -d "$1" ]
+then
+  echo "Error"
+  exit 2
+
+else
+  ls -l -S
+fi
