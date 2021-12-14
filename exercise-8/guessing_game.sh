ca98568adcf91a2d8262bf3cb92e749e58250d3e
Matteo Floirat <matteo.floirat@edu.devinci.fr>
2021-12-14 13:59:21 +0100

    exo 8

--- exercise-8/guessing_game.sh
+++ exercise-8/guessing_game.sh
@@ -1,18 +1,19 @@
 #! /bin/bash
-echo "At any time , hit CTRL + C to exit the script"
-number_to_guess=$(($RANDOM%100))
-while ((guess != number_to_guess)) ; do
-	read -p "" guess # Asks the user for a number and put it in the variable "guess"
-	echo Number given by user : $gues
-	
-	if ! (($guess =~[0-9]))
+echo "At any time, hit CTRL + C to exit the script"
+number_to_guess=$(($RANDOM%100+1))
+echo "I have in mind a number in between 1 and 100, can you find it?"
+
+while true; do
+	read -p"" "Number giver by user : " guess  # Asks the user for a number and put it in the variable "guess"
+
+	if ! [[ $guess =~ ^[0-9]+$ ]];
 	then
-		echo "Erreur, choose a number"
+		echo "Error, choose a number"
 
 	elif (($guess<$number_to_guess))
 	then
 		echo "The number to guess is higher"
-	
+
 	elif (($guess>$number_to_guess))
 	then
 		echo "The number to guess is lower"
@@ -20,6 +21,6 @@ while ((guess != number_to_guess)) ; do
 	elif (($guess==$number_to_guess))
 	then
 		echo "You just found the number, it was indeed $number_to_guess"
-		exit 
+		exit
 	fi
 done
